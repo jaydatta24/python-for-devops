7e13705aa3dfd182c52240b5106148093029e5ac
jaydatta24 <mohitejaydatta123@gmail.com>
2026-03-26 15:12:16 +0530

    Practing python basics programs

---
+++ Day-02/examples/02-float-1.py
@@ -0,0 +1,19 @@
+num1 = 5.3
+num2 = 6.54
+
+result = num1 + num2
+print ("The float addition result is: ",result )
+
+result1 = num1 - num2
+print ("The float substraction result is: ",result1 )
+
+result2 = num1 / num2
+print ("The float division result is: ",result2 )
+
+result3 = num1 * num2
+print ("The float multiplication result is: ",result3 )
+
+# Rounfing function
+
+result5 = round(3.94484787, 1)
+print("the result of round dunction is : ", result5)
